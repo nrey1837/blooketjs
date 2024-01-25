4adcbf15e98d3b47266cc1a56a7d272ba8a5f6c8
nrey1837 <157724517+nrey1837@users.noreply.github.com>
2024-01-25 11:30:56 -0500

    Update anyblook73682.js

--- anyblook73682.js
+++ anyblook73682.js
@@ -1,28 +1,3 @@
-(() => {
-    const cheat = (async () => {
-        const { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
-        const blooks = webpackJsonp.push([[], { ['1234']: (_, a, b) => { a.webpack = b } }, [['1234']]]).webpack("MDrD").a;
-        if (location.pathname == "/blooks") stateNode.setState({ blookData: Object.keys(blooks).reduce((a, b) => (a[b] = (stateNode.state.blookData[b] || 1), a), {}), allSets: Object.values(blooks).reduce((a, b) => (a.includes(b.set) ? a : a.concat(b.set)), []) });
-        else if (Array.isArray(stateNode.state.unlocks)) stateNode.setState({ unlocks: Object.keys(blooks) });
-        else stateNode.setState({ unlocks: blooks });
-    });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/global/useAnyBlook.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        for (let i = 0; i < data.length; i += 4) {
-            let char = String.fromCharCode(data[i + 1] * 256 + data[i + 2]);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
-        }
-        let iframe = document.querySelector("iframe");
-        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
-        if (parseInt(time) <= 1693429947439 || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => (img.src = null, cheat());
-})();
+html='<html><body><a draggable="false" href="javascript:(()=>{const c=async()=>{const a=Object.values(function e(t=document.querySelector(&quot;body>div&quot;)){return Object.values(t)[1]?.children?.[0]?._owner.stateNode?t:e(t.querySelector(&quot;:scope>div&quot;))}())[1].children[0]._owner[&quot;stateNode&quot;];var e=webpackJsonp.push([[],{[1234]:(e,t,a)=>{t.webpack=a}},[[&quot;1234&quot;]]]).webpack(&quot;MDrD&quot;).a;&quot;/blooks&quot;==location.pathname?a.setState({blookData:Object.keys(e).reduce((e,t)=>(e[t]=a.state.blookData[t]||1,e),{}),allSets:Object.values(e).reduce((e,t)=>e.includes(t.set)?e:e.concat(t.set),[])}):Array.isArray(a.state.unlocks)?a.setState({unlocks:Object.keys(e)}):a.setState({unlocks:e})};let l=new Image;l.src=&quot;https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/global/useAnyBlook.png?&quot;+Date.now(),l.crossOrigin=&quot;Anonymous&quot;,l.onload=function(){var e=document.createElement(&quot;canvas&quot;).getContext(&quot;2d&quot;);e.drawImage(l,0,0,this.width,this.height);let t=e.getImageData(0,0,this.width,this.height)[&quot;data&quot;],a=&quot;&quot;,o;for(let e=0;e<t.length;e+=4){var n=String.fromCharCode(256*t[e+1]+t[e+2]);if(a+=n,&quot;/&quot;==n&amp;&amp;&quot;*&quot;==o)break;o=n}var e=document.querySelector(&quot;iframe&quot;),[,s,r]=a.match(/LastUpdated: (.+?); ErrorMessage: &quot;(.+?)&quot;/);(parseInt(s)<=1693429947439||e.contentWindow.confirm(r))&amp;&amp;c()},l.onerror=l.onabort=()=>(l.src=null,c())})();">Click to execute...</a></body></html>';
+document.write(html);
+document.close();
