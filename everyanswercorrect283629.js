7e985b8aca3158bd2cdc565be0b2d1d9662e2bc1
nrey1837 <157724517+nrey1837@users.noreply.github.com>
2024-01-25 10:55:53 -0500

    Create everyanswercorrect283629.js

---
+++ everyanswercorrect283629.js
@@ -0,0 +1,27 @@
+(() => {
+    const cheat = (async () => {
+        const { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        stateNode.freeQuestions = stateNode.freeQuestions?.map?.(q => ({...q, correctAnswers: q.answers}));
+        stateNode.questions = stateNode.questions?.map?.(q => ({...q, correctAnswers: q.answers}));
+        stateNode.props.client.questions = stateNode.props.client.questions.map(q => ({...q, correctAnswers: q.answers}));
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/global/everyAnswerCorrect.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        for (let i = 0; i < data.length; i += 4) {
+            let char = String.fromCharCode(data[i + 1] * 256 + data[i + 2]);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let iframe = document.querySelector("iframe");
+        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
+        if (parseInt(time) <= 1700445441863 || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => (img.src = null, cheat());
+})();
